97cc60b9e8d76c54e7de8410037c2cbf2931acbb
raphael.vicente <raphael.vicente@koin.com.br>
2022-09-06 17:55:19 -0300

    estrutura sequencial exercicio 1 resolvido

--- Seção 5/EstruturaSequencialExercicio1/src/Main.java
+++ Seção 5/EstruturaSequencialExercicio1/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int valorA, valorB, soma;
+
+        valorA = sc.nextInt();
+        valorB = sc.nextInt();
+        soma = valorA + valorB;
+
+        System.out.println("SOMA = " + soma);
+
+        sc.close();
+
     }
 }
