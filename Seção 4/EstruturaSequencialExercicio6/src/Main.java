1d7969b77c343f13769c1e0bbbfa442dd32aa787
raphael.vicente <raphael.vicente@koin.com.br>
2022-09-09 09:43:53 -0300

    inclusão de pastas e exercicios resolvidos

--- Seção 4/EstruturaSequencialExercicio6/src/Main.java
+++ Seção 4/EstruturaSequencialExercicio6/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, C;
+
+
     }
 }
